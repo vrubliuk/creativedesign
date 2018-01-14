c247b029ad4d40a5fc16fa98d2806a2b086c1da6
vrubliuk <vrubliuk@gmail.com>
2018-01-14 18:44:42 +0200

    Fix menu. Add warning regarding IE

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,26 @@
 ﻿$(document).ready(function () {
+  // CHECK IF BROWSER IS IE
+  function isIE() {
+    var ua = navigator.userAgent;
+    var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+    return is_ie;
+  }
+  if (isIE()) {
+    var cover = document.getElementById("ie-cover");
+    cover.style.display = "block";
+    document.body.style.overflowY = "hidden";
+  } 
+
+  // HEADER
+  window.onscroll = () => {
+    var header = document.getElementsByTagName("header")[0];
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      header.style.boxShadow = "0 8px 16px 0 rgba(0, 0, 0, 0.2)";
+    } else {
+      header.style.boxShadow = "";
+    }
+}
+
   // SMOOTH SCROLLING ANCHOR LINKS
   $("a").on('click', function (event) {
     if (this.hash !== "") {
@@ -17,7 +39,7 @@
       hH = $('#about').outerHeight(),
       wH = $(window).height(),
       wS = $(this).scrollTop();
-    if (wS > (hT + hH - wH) && (hT > wS) && (wS + wH > hT + hH)) {
+    if (wS > (hT+hH-wH)) {
       document.getElementsByClassName('about-scale')[0].style.width = "79%";
       document.getElementsByClassName('about-scale')[1].style.width = "90%";
       document.getElementsByClassName('about-scale')[2].style.width = "69%";
